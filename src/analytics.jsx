9a08feed603292393dad3235455bcb9b3ebfb297
m.robatjazi <43264216+mahdi-robatjazi78@users.noreply.github.com>
2024-01-02 16:41:14 +0330

    Fix Speed Inshight Vercel Bug

--- src/analytics.jsx
+++ src/analytics.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const VercelAnalyticsComponent = ({children}) => {
   return (
