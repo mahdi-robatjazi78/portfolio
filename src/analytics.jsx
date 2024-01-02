0f63cd04aee761ff223f68280ab185791e6f4ef1
m.robatjazi <43264216+mahdi-robatjazi78@users.noreply.github.com>
2024-01-02 16:35:44 +0330

    handle Vercel Speed Insight In Project

--- src/analytics.jsx
+++ src/analytics.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const VercelAnalyticsComponent = ({children}) => {
   return (
     <>
       {children}
       <Analytics />
+      <SpeedInsights />
   </>
   )
 }
